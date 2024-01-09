d8d3b893e4d7051bff08dd79c8c0e773e1956ba3
George Glyn <george@sparklingapps.com>
2024-01-09 15:12:33 +0530

    Added current year in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-gradient-to-r from-yellow-800 via-orange-500 to-yellow-800 text-white py-4 text-center sm:flex justify-between p-6">
       <p className="mb-4 sm:mb-0">
-        &copy; 2023{' '}
+        &copy; {currentYear}{' '}
         <a
           href="https://www.tempeh.today"
           className="text-blue-300 hover:text-blue-100"
